9c069d7f87ae17321dd810fe7457836fc13aa599
hoymkot <hkot@tyreplus.com.cn>
2019-11-28 16:57:37 -0800

    create packages

---
+++ server/handle.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+# filename: handle.py
+
+import hashlib
+import web
+import reply
+import receive
+
+class Handle(object):
+    def GET(self):
+        try:
+            data = web.input()
+            if len(data) == 0:
+                return "hello, this is handle view"
+            signature = data.signature
+            timestamp = data.timestamp
+            nonce = data.nonce
+            echostr = data.echostr
+            token = "hello" #请按照公众平台官网\基本配置中信息填写
+
+            l = [token, timestamp, nonce]                       
+            l.sort()
+            hashcode = self.hashgen(l);
+            print ("handle/GET func: hashcode, signature: ", hashcode, signature)
+            if hashcode == signature:
+                return echostr
+            else:
+                return ""
+        except Exception as Argument:
+            return Argument
+
+    def hashgen(object, t):
+        print(t)
+        sha1 = hashlib.sha1()
+        map(sha1.update, t);
+        code = sha1.hexdigest();
+        print(code);
+        return code;
+
+
+    def POST(self):
+        try:
+            webData = web.data()
+            print("Handle Post webdata is ", webData)
+   #后台打日志
+            recMsg = receive.parse_xml(webData)
+            if isinstance(recMsg, receive.Msg):
+                toUser = recMsg.FromUserName
+                fromUser = recMsg.ToUserName
+                if recMsg.MsgType == 'text':
+                    content = "test"
+                    replyMsg = reply.TextMsg(toUser, fromUser, content)
+                    return replyMsg.send()
+                if recMsg.MsgType == 'image':
+                    mediaId = recMsg.MediaId
+                    replyMsg = reply.ImageMsg(toUser, fromUser, mediaId)
+                    return replyMsg.send()
+                if isinstance(recMsg, receive.EventMsg):
+                    if recMsg.Event == 'CLICK':
+                        if recMsg.Eventkey == 'mpGuide':
+                            content = u"编写中，尚未完成".encode('utf-8')
+                            replyMsg = reply.TextMsg(toUser, fromUser, content)
+                        return replyMsg.send()
+            else:
+                print ("暂且不处理")
+                return "success"
+        except Exception as Argment:
+            return Argment
